8c410b677da9324e7031d2b43792ec30ae425e6e
zz <517669936@qq.com>
2021-07-23 20:24:27 +0800

    version update

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from './time-lock-single-tx';
 export * from './timelock-multi-tx';
 export * from './signer';
 export * from './types';
-export * from './config';
+export {RunningConfig} from './config';
 export {Builder, Signer, Address} from '@lay2/pw-core';
 export * from './address';
+export {CKBEnv} from "./helpers";
